1b4b3000f0d92ad991bade8675325c687ed45ee2
typist1 <raymsun1@gmail.com>
2026-04-21 11:11:53 -0500

    Added more resume routes

--- src/routes/resumeRoutes.js
+++ src/routes/resumeRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 20 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const allowed = [
       'application/pdf',
